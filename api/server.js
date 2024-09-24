799a0d36647fc4129eeda9f6b3e79648e0d197c3
Kseniia Kondrat <oootlichno@gmail.com>
2024-09-24 16:10:50 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const usersRouter = require('../api/users/users-router')
-const authRouter = require('../api/auth/auth-router')
+const usersRouter = require('../api/users/users-router');
+const authRouter = require('../api/auth/auth-router');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const knex = require('../data/db-config');
+
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -13,17 +17,37 @@ const authRouter = require('../api/auth/auth-router')
   Users that do authenticate should have a session persisted on the server,
   and a cookie set on the client. The name of the cookie should be "chocolatechip".
 
-  The session can be persisted in memory (would not be adecuate for production)
+  The session can be persisted in memory (would not be adequate for production)
   or you can use a session store like `connect-session-knex`.
  */
 
 const server = express();
 
+server.use(session({
+  name: 'chocolatechip', 
+  secret: 'shh', 
+  saveUninitialized: false, 
+  resave: false, 
+  store: new KnexSessionStore({ 
+    knex,
+    createTable: true, 
+    clearInterval: 1000 * 60 * 10, 
+    tablename: 'sessions', 
+    sidfieldname: 'sid', 
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 10, 
+    secure: false, 
+    httpOnly: true, 
+    // sameSite: 'none', 
+  }
+})); 
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-server.use('/api/users', usersRouter)
-server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
@@ -37,3 +61,4 @@ server.use((err, req, res, next) => { // eslint-disable-line
 });
 
 module.exports = server;
+
